d25c27a6e3e3319fc8e30110773e152a7fcaaec2
adamblatchford <phil.baxter@equiteq.com>
2023-06-01 14:11:29 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,20 @@
 import os
 from flask import Flask
+from flask import request
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/api/trigger', methods=['POST'])
+def handle_trigger():
+    data = request.get_json()
+    # Process the data and perform necessary actions
+    response = {'message': 'Trigger received', 'data': data}
+    return jsonify(response)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
