f8952cc5aa4ecc2d125e250692a18321d8b3090d
adamblatchford <phil.baxter@equiteq.com>
2023-06-02 09:37:56 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -57,19 +57,39 @@ def hello_world():
 @app.route('/api/trigger', methods=['POST'])
 
 def handle_trigger():
-    secret = request.headers.get('Authorization')
+	
+	secret = request.headers.get('Authorization')
+
+	openai_api_key=''
+	synopsis =''
+	target_description = ''
+	recordID = ''
+	
+	if secret is None:
+		return 'Unauthorized', 401
+	
+	data = request.get_json()
     
-    if secret is None:
-        return 'Unauthorized', 401
-    
-    data = request.get_json()
-    # Extract key-value pairs from the data dictionary
-    for key, value in data.items():
-        print(f"Key: {key}, Value: {value}")
-        # Perform further processing as needed
-    
-    response = {'data': data, 'secret': secret}
-    return jsonify(response)
+	# Extract key-value pairs from the API data dictionary
+	
+	for key, value in data.items():
+		# print(f"Key: {key}, Value: {value}")
+		# Perform further processing as needed
+		
+		if key == 'synopsis':
+			synopsis = value
+		
+		if key == 'target_description':
+			target_description = value
+		
+		if key == 'hs_object_id':
+			recordID = value
+		
+		if key == 'openAIkey':
+			openai_api_key = value
+		
+	response = {'synopsis': synopsis, 'target_description': target_description, 'hs_object_id':  hs_object_id, 'openAIkey': openai_api_key, 'secret': secret}
+	return jsonify(response)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
