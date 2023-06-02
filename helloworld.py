548812245656c2e2100b6647fc6c9433e1097748
adamblatchford <phil.baxter@equiteq.com>
2023-06-02 11:19:17 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -53,7 +53,7 @@ def handle_trigger():
 	sort = ''
 	query = ''
 	properties = ['hs_object_id', 'description','specialities__linkedin_', 'linkedin_description','level_2_taxonomy','level_3_taxonomy','web_home_page___ai_scrape','domain']
-	limit = 5
+	limit = 2
 	after = 0
 	
 	publicObjectSearchRequest = {
