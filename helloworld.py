c21e10fddcf113e9676b94b32e2392a400ed1ce3
adamblatchford <phil.baxter@equiteq.com>
2023-06-02 10:08:41 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -42,8 +42,66 @@ def handle_trigger():
 		
 		if key == 'openAIkey':
 			openai_api_key = value
+	
+	client = hubspot.Client.create(access_token=hubspot_app_token)
+	filter = { 'propertyName': 'lifecyclestage', 'operator': 'EQ', 'value': 'salesqualifiedlead'}
+	filterGroup = { 'filters': [filter] }
+	sort = ''
+	query = ''
+	properties = ['description','specialities__linkedin_', 'linkedin_description','level_2_taxonomy','level_3_taxonomy','web_home_page___ai_scrape','domain']
+	limit = 1
+	after = 0
+	
+	publicObjectSearchRequest = {
+    		'filterGroups': [filterGroup],
+    		'sorts': [sort],
+		'query': query,
+		'properties': properties,
+		'limit': limit,
+		'after': after
+	};
+	
+	try:
+		api_response = client.crm.companies.search_api.do_search(publicObjectSearchRequest)
 		
-	response = {'synopsis': synopsis, 'target_description': target_description, 'hs_object_id':  recordID, 'openAIkey': openai_api_key, 'secret': secret}
+		except ApiException as e:
+			print("Exception when calling basic_api->get_page: %s\n" % e)
+	
+	i = 0
+	
+	for thiscompany in api_response.results:
+		
+		i = i + 1
+		
+		domain = thiscompany.properties['domain']
+		if domain is None:
+			domain =''
+		
+		description = thiscompany.properties['description']
+		if description is None:
+			description =''
+		
+		linkedin_specialities = thiscompany.properties['specialities__linkedin_']
+		if linkedin_specialities is None:
+			linkedin_specialities = ''
+		
+		linkedin_description = thiscompany.properties['linkedin_description']
+		if linkedin_description is None:
+			linkedin_description =''
+			
+		level2 = thiscompany.properties['level_2_taxonomy']
+		if level2 is None:
+			level2 = ''
+		
+		level3 = thiscompany.properties['level_3_taxonomy']
+		if level3 is None:
+			level3 = ''
+		
+		webscrape = thiscompany.properties['web_home_page___ai_scrape']
+		if webscrape is None:
+			webscrape = ''
+
+	response = {'counter': i, 'synopsis': synopsis, 'target_description': target_description, 'hs_object_id':  recordID, 'openAIkey': openai_api_key, 'secret': secret}
 	return jsonify(response)
 
 if __name__ == '__main__':
